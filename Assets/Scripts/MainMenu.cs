5c83f15d8b32bc76cda0b182f38ab6070313b3f3
bidoumou <103275212+bidoumou@users.noreply.github.com>
2022-12-15 09:51:58 +0100

    Functional Level Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
+    
+    public GameObject LevelNumber;
+
+    public void PlayLevel(int level){
+
+        //levelNumber = LevelNumber.GetComponent<TextMeshProUGUI>().text;
+        //Debug.Log(levelNumber);
+        SceneManager.LoadScene(level);
     }
 
     public void QuitGame(){
-
         Debug.Log("Le jeu s'est arrêté");
         Application.Quit();
     }
+    
 }
